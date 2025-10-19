5e055e2d8fd166dbb53d1664f4488af062504b25
rkk5603 <16rgkelly@gmail.com>
2025-10-19 10:15:29 -0400

    Update align.mk

--- Week8/align.mk
+++ Week8/align.mk
@@ -16,8 +16,6 @@ NAME=ebola
 # SRR number
 SRR=SRR1972976
 
-# SRR from a pacbio long read SRR1640364 (week 5 hw)
-
 # Reference genome
 REF=refs/${NAME}.fa
 
@@ -30,8 +28,12 @@ R2=reads/${SRR}_2.fastq
 #reports directory
 REPORTS=reports
 
+
+#Sample name is SRR by defualt unless specified
+SAMPLE=${SRR}
+
 # BAM file
-BAM=bam/${SRR}.bam
+BAM=bam/${SAMPLE}.bam
 
 # How many reads to download
 N=100000
@@ -42,80 +44,80 @@ ACC=AF086833
 
 # for bigwig file
 # The temporary bedgraph file
-BG=bam/${SRR}.bedgraph
+BG=bam/${SAMPLE}.bedgraph
 
 # The BW wiggle file
-BW=bam/${SRR}.bw
+BW=bam/${SAMPLE}.bw
 
 
 #---------------------the stuff---------------------------------------
 
+
 # Obtain the reference genome
-
-test:
-  echo ${SRR}
-
 fetch:
 
-	#makes ref directory
-	mkdir -p $(dir ${REF})
+        #makes ref directory
+        mkdir -p $(dir ${REF})
 
-	#gets ebola genome genome
-	bio fetch ${ACC} --format fasta > ${REF}
+        #gets ebola genome genome
+        bio fetch ${ACC} --format fasta > ${REF}
 
-	#displays genome stats
-	seqkit stats ${REF}
+        #displays genome stats
+        seqkit stats ${REF}
 
 #Download subset of data for 100x coverage (10000 reads)
 #Remove -X for all data
 fastq:
-	# Create the reads directory
-	mkdir -p $(dir ${R1})
+        # Create the reads directory
+        mkdir -p $(dir ${R1})
 
-	# Download the reads
-	fastq-dump -X ${N} --outdir reads --split-files ${SRR}
-	# Show some information about the reads
-	seqkit stats ${R1} ${R2}
+        # Download the reads
+        fastq-dump -X ${N} --outdir reads --split-files ${SRR}
+        # Show some information about the reads
+        seqkit stats ${R1} ${R2}
 
 fastqc:
-	mkdir -p ${REPORTS}
-	fastqc -o ${REPORTS} ${R1} ${R2}
+        mkdir -p ${REPORTS}
+        fastqc -o ${REPORTS} ${R1} ${R2}
 
 # Index the reference genome
 index:
-	bwa index ${REF}
+        bwa index ${REF}
 
 # Align the reads and convert to BAM. Use 4 threads
 # Works for paired-end reads. Modify for single-end reads.
 align:
-	# Make the BAM directory
-	mkdir -p $(dir ${BAM})
+        # Make the BAM directory
+        mkdir -p $(dir ${BAM})
 
-	# Align the reads
-	bwa mem -t 4 ${REF} ${R1} ${R2} | samtools sort  > ${BAM}
+        # Align the reads
+        bwa mem -t 4 ${REF} ${R1} ${R2} | samtools sort  > ${BAM}
 
-	# Index the BAM file
-	samtools index ${BAM}
+        # Index the BAM file
+        samtools index ${BAM}
 
 # Generate alignment statistics
 stats:
-	samtools flagstat ${BAM}
+        samtools flagstat ${BAM}
+
+#generates a BW version of the BAM file
+bigwig:
+
+        # Index the reference genome
+        samtools faidx ${REF}
+
+        # Generate the temporary bedgraph file.
+        LC_ALL=C; bedtools genomecov -ibam ${BAM} -split -bg | \
+        sort -k1,1 -k2,2n > ${BG}
+
+        # Convert the bedgraph file to bigwig.
+        bedGraphToBigWig ${BG} ${REF}.fai ${BW}
+
 
 # Clean up generated files
 clean:
-	rm -rf ${REF} ${R1} ${R2} ${BAM} ${BAM}.bai
+        rm -rf ${REF} ${R1} ${R2} ${BAM} ${BAM}.bai bam/${SAMPLE}.bedgraph
 
-bigwig:
-
-	# Index the reference genome
-	samtools faidx ${REF}
-
-	# Generate the temporary bedgraph file.
-	LC_ALL=C; bedtools genomecov -ibam ${BAM} -split -bg | \
-	sort -k1,1 -k2,2n > ${BG}
-
-	# Convert the bedgraph file to bigwig.
-	bedGraphToBigWig ${BG} ${REF}.fai ${BW}
 
 # Create necessary directories
 all: fetch fastq index align stats bigwig
